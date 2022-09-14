2fa46576a028454873c6e1761fcf648d24d25ead
thathsara senarathne <yasithsenaz2014@gmail.com>
2022-09-14 10:39:43 +0530

    initial commit

---
+++ src/main/java/com/example/cuitask/CuiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.cuitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuiTaskApplication.class, args);
+	}
+
+}
